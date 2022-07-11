030e074ac0474c131d6d2006b3cd107d8a759574
hyesunie <hyesunie1025@gmail.com>
2022-07-11 22:33:17 +0900

    day: 2022년 7월 11일

---
+++ front/components/AppLayout.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { Input, Menu, Row, Col } from "antd";
+import "antd/dist/antd.css";
+import styled from "styled-components";
+import UserProfile from "../components/UserProfile";
+import LoginForm from "../components/LoginForm";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
+//이거 아니면 useMemo로 빼면 된다
+
+const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">노드버드</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">프로필</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <SearchInput enterButton />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">회원가입</Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://lizard-cosmos-28e.notion.site/Front-end-Developer-e6c9d68415b64975bf5f6ffa975d04cf"
+            target="_blank"
+            rel="noreferrer noopenner"
+          >
+            Made by hyesun
+          </a>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
---
+++ front/pages/index.js
@@ -0,0 +1,11 @@
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Welcome to Next.js!</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
