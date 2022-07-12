4630b38f5224ed261ae36fa848c20fbf29fb6abc
hyesunie <hyesunie1025@gmail.com>
2022-07-12 22:50:34 +0900

    day: 2022년 7월12일

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,5 +1,29 @@
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+import FollowerList from "../components/FollowerList";
+import FollowingList from "../components/FollowingList";
+import NicknameEditForm from "../components/NicknameEditForm";
+
 const Profile = () => {
-  return <div>프로필 페이지</div>;
+  const followingList = [
+    { nickname: "아이유" },
+    { nickname: "소지섭" },
+    { nickname: "아이브" },
+  ];
+  const followerList = [{ nickname: "쏠" }, { nickname: "백예린" }];
+
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowingList header="팔로잉 목록" data={followingList} />
+        <FollowerList header="팔로워 목록" data={followerList} />
+      </AppLayout>
+    </>
+  );
 };
 
 export default Profile;
